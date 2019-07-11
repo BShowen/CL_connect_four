20b95195fdb24ca94810f432ee872252278fc170
Bradley Showen <BShowen@me.com>
2019-07-11 17:36:53 -0400

    green light on tests

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,9 +1,27 @@
 class GameBoard
-    def board
+    def display
         "|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|".center(100)
     end
-end
 
-class Sqaure
-    
-end 
+    def initialize
+        @board = [[nil,nil,nil,nil,nil,nil],
+                            [nil,nil,nil,nil,nil,nil],
+                            [nil,nil,nil,nil,nil,nil],
+                            [nil,nil,nil,nil,nil,nil],
+                            [nil,nil,nil,nil,nil,nil],
+                            [nil,nil,nil,nil,nil,nil]]
+    end
+
+    def row(row)
+        @board[5 - row]
+    end
+
+    def row_column(column)
+        col = []
+        for index in 0..5 do 
+           col << self.row(index)[column]
+        end
+        col
+    end
+
+end
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -1,7 +1,20 @@
-require "./game_board.rb"
+require "./lib/game_board.rb"
 
 RSpec.describe GameBoard do
-    let(:board) {GameBoard.new}
+    before(:each) do
+        @board = GameBoard.new
+    end
 
-    context ""
+    context "board rows" do
+        it "returns the row specified in the argument" do
+            expect(@board.row(0)).to eql([nil,nil,nil,nil,nil,nil])
+        end
+    end
+
+    context "board columns" do
+        it "returns the colum specified in the argument" do
+            expect(@board.row_column(5)).to eql([nil,nil,nil,nil,nil,nil])
+        end
+    end
+
 end
