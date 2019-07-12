14ab5a5643eeab958ab198c44912979fd37d0a62
Bradley Showen <BShowen@me.com>
2019-07-11 22:01:21 -0400

    essential methods work properly

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,27 +1,47 @@
+# require_relative "./square.rb"
+
 class GameBoard
-    def display
-        "|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|\n|   |   |   |   |   |   |   |\n|---|---|---|---|---|---|---|".center(100)
-    end
 
     def initialize
-        @board = [[nil,nil,nil,nil,nil,nil],
-                            [nil,nil,nil,nil,nil,nil],
-                            [nil,nil,nil,nil,nil,nil],
-                            [nil,nil,nil,nil,nil,nil],
-                            [nil,nil,nil,nil,nil,nil],
-                            [nil,nil,nil,nil,nil,nil]]
+        @board = Array.new(6) do
+            Array.new(7," ")
+        end
+    end
+
+    def display
+        @board.each do |row|
+            puts "| " + row.join(" | ") + " |"
+        end
+        puts "|---|---|---|---|---|---|---|"
     end
 
     def row(row)
         @board[5 - row]
     end
 
-    def row_column(column)
+    def column(column)
         col = []
         for index in 0..5 do 
            col << self.row(index)[column]
         end
         col
+    end
+
+    def assign_column(specified_column, character)
+        return nil if column_full?(specified_column)
+        
+        for index in 0..6 do
+            if row(index)[specified_column] == " "
+                row(index)[specified_column] = character
+                break   
+            end
+        end
+    end
+
+    def column_full?(column)
+        column(column).all? do |square|
+            square != " "
+        end
     end
 
 end
