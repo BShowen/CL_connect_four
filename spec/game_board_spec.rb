6979004abf1a898403decf37abbd30fd883bfc1a
Bradley Showen <BShowen@me.com>
2019-07-11 22:01:45 -0400

    all tests are green

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -5,15 +5,47 @@ RSpec.describe GameBoard do
         @board = GameBoard.new
     end
 
-    context "board rows" do
-        it "returns the row specified in the argument" do
-            expect(@board.row(0)).to eql([nil,nil,nil,nil,nil,nil])
+    def fill_column_zero
+        6.times do
+            @board.assign_column(0,"X")
         end
     end
 
-    context "board columns" do
+    context "row" do
+        it "returns the row specified in the argument" do
+            expect(@board.row(0)).to eql([" "," "," "," "," "," "," "])
+        end
+    end
+
+    context "column" do
         it "returns the colum specified in the argument" do
-            expect(@board.row_column(5)).to eql([nil,nil,nil,nil,nil,nil])
+            expect(@board.column(5)).to eql([" "," "," "," "," "," "])
+        end
+    end
+
+    context "column_full?" do 
+        it "returns false when a column isnt full" do
+            expect(@board.column_full?(0)).to eql(false)
+        end
+
+        it "returns true for a full column" do
+            fill_column_zero
+
+            expect(@board.column_full?(0)).to eql(true)
+        end
+    end
+
+    context "assign_column" do 
+        it "assigns a value to the bottom most square in the column" do
+            @board.assign_column(6,"X")
+            @board.assign_column(6,"Y")
+            expect(@board.row(1)[6]).to eql("Y")
+        end
+
+        it "returns nil if column is full" do 
+            fill_column_zero
+
+            expect(@board.assign_column(0,"x")).to eql(nil)
         end
     end
 
