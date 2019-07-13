5657a72c3f3dc4c4b253722d94aee16150297f52
Bradley Showen <BShowen@me.com>
2019-07-13 09:31:01 -0400

    refactor methods

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -2,72 +2,77 @@ require_relative "./square.rb"
 
 class GameBoard
 
+    attr_accessor :cloned_board
+
     def initialize
-        @board = Array.new(6) { Array.new(7) }
+        @game_board = Array.new(6) { Array.new(7) }
         initialize_each_sqaure
-        @display_board = []
-    end
-
-    def initialize_each_sqaure
-        @board.map! do |row|
-            row.map! do 
-                Square.new
-            end
-        end
-    end
-
-    def convert_object_board_into_readable_board
-        @display_board.clear
-        # for row in 0...(@board.length) do
-        #     @display_board << []
-        #     for square in 0...(@board[row].length) do
-        #         @display_board[row] << @board[row][square].game_piece
-        #     end
-        # end
-        @display_board = Marshal.load(Marshal.dump(@board))
-        @display_board.each do |row|
-            row.map! do |square|
-                square.game_piece
-            end
-        end
+        @cloned_board = nil
     end
 
     def display
-        convert_object_board_into_readable_board
-        @display_board.each do |row|
-            puts "| " + row.join(" | ") + " |"
-        end
-        puts "|---|---|---|---|---|---|---|"
+        deep_clone_object_board
+        convert_cloned_board_into_string_board
+        add_borders_to_board_columns
+        @cloned_board.join
     end
 
-    def row(row)
-        @board[5 - row]
+    def get_row(row)
+        row = (5 - row) # read grid from the bottom to the top.
+        @game_board[row]
     end
 
-    def column(column)
+    def get_column(column)
         col = []
         for index in 0..5 do 
-           col << self.row(index)[column]
+           col << self.get_row(index)[column]
         end
         col
     end
 
-    def assign_column(specified_column, character)
-        return nil if column_full?(specified_column)
+    def drop_piece(specified_column, new_character)
+        return false if column_full?(specified_column)
         
         for index in 0..6 do
-            if row(index)[specified_column].game_piece == " "
-                row(index)[specified_column].game_piece = character
+            if get_row(index)[specified_column].game_piece == " "
+                get_row(index)[specified_column].game_piece = new_character
                 return true
                 break   
             end
         end
     end
+    
 
-    def column_full?(column)
-        self.column(column).all? do |square|
-            square.game_piece != " "
+    private 
+
+    def initialize_each_sqaure
+        @game_board.map! do |row|
+            row.map! { Square.new }
         end
     end
 
+    def convert_cloned_board_into_string_board
+        @cloned_board.each do |row|
+            row.map!(&:game_piece)
+        end
+    end
+
+    def add_borders_to_board_columns
+        @cloned_board.map! do |row|
+            "| " + row.join(" | ") + " |\n"
+        end
+
+        bottom_row = ["|---|---|---|---|---|---|---|"]
+        @cloned_board << bottom_row
+    end
+
+    def deep_clone_object_board
+        @cloned_board = Marshal.load(Marshal.dump(@game_board))
+    end
+
+    def column_full?(column)
+        self.get_column(column).all? do |square|
+            square.game_piece != " "
+        end
+    end
 end
