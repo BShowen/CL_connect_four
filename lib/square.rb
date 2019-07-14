63a4602651bc887aac657b630da88bc166f5e422
Bradley Showen <BShowen@me.com>
2019-07-14 09:58:42 -0400

    class square now has neighbors

--- lib/square.rb
+++ lib/square.rb
@@ -1,13 +1,21 @@
 class Square
 
-    attr_accessor :character
+    attr_accessor :character, :neighbor
 
     def initialize
         @character = " "
+        @neighbor = {  :top => nil,
+                                    :bottom => nil, 
+                                    :left => nil,
+                                    :right => nil,
+                                    :top_left => nil,
+                                    :top_right => nil,
+                                    :bottom_left => nil,
+                                    :bottom_right => nil
+                                }
     end
 
     def is_unassigned?
         @character == " "
     end
-
 end 
