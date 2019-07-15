5e213244f62d139f4c9ca5c530d288274e6ca2e4
Bradley Showen <BShowen@me.com>
2019-07-14 21:32:17 -0400

    green light. refactor tests for better overall comprehension

--- spec/game_ref_spec.rb
+++ spec/game_ref_spec.rb
@@ -9,75 +9,39 @@ RSpec.describe "GameBoard module" do
         @game_board = GameBoard.new(player1, player2)
     end
     
-    def create_winner_scenario
-        for i in 0..3 do
-            @game_board.drop_piece(i,"X")
+    def create_winner_in_this_row(row)
+        for column in 2..5
+            @game_board.get_row(row)[column].character = "X"
         end
     end
 
-    def create_a_top_row_winner
-        for i in 0..4 do   
-            for x in 0..6 do
-                @game_board.drop_piece(x,x.to_s)
-            end
+    def create_winner_in_this_column(column)
+        4.times do
+            @game_board.drop_piece(column,"X")
         end
-        for i in 3..6
-            @game_board.drop_piece(i,"X")
-        end
-    end
-
-    def make_player
-        player1 = double('player1')
-        allow(player1).to receive(:name){"John Smith"}
-        allow(player1).to receive(:character){"X"}
     end
 
     context "#winner?" do
-        it "returns the character of the winner" do 
-            create_winner_scenario
+        it "returns the character of the winner in a random row" do 
+            create_winner_in_this_row(rand(0..5))
+            puts @game_board.display
+            expect(@game_board.winner?).to eql("X")
+        end
+
+        it "returns the character of the winner in a random column" do 
+            create_winner_in_this_column(rand(0..6))
+            puts @game_board.display
             expect(@game_board.winner?).to eql("X")
         end
 
         it "returns false when there is no winner: the board is blank" do
             expect(@game_board.winner?).to eql(false)
         end
-
-        it "finds a winner at the end of a row" do 
-            @game_board.drop_piece(3,"X")
-            @game_board.drop_piece(4,"X")
-            @game_board.drop_piece(5,"X")
-            @game_board.drop_piece(6,"X")
-            
-            expect(@game_board.winner?).to eql("X")
-        end
-
-        it "finds a winner in a different row" do
-            @game_board.drop_piece(0,"X")
-            @game_board.drop_piece(2,"X")
-            @game_board.drop_piece(4,"X")
-            @game_board.drop_piece(6,"X")
-            @game_board.drop_piece(1,"O")
-            @game_board.drop_piece(3,"O")
-            @game_board.drop_piece(5,"O")
-            @game_board.drop_piece(2,"Q")
-            @game_board.drop_piece(3,"Q")
-            @game_board.drop_piece(4,"Q")
-            @game_board.drop_piece(5,"Q")
-
-            expect(@game_board.winner?).to eql("Q")
-        end
-        
-        it "finds a winner in the rop row" do    
-            create_a_top_row_winner
-
-            expect(@game_board.winner?).to eql("X")
-        end
     end
+
 
     context "#winners_name" do
         xit "returns the name of the winner" do 
-            make_player
-
             expect(@game_board.winners_name).to eql("John")
         end
     end
