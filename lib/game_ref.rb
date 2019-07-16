5d182c0410a4157c6304b4e6aa3ef96a5034950f
Bradley Showen <BShowen@me.com>
2019-07-15 21:59:42 -0400

    refactor to achieve green light tests

--- lib/game_ref.rb
+++ lib/game_ref.rb
@@ -1,15 +1,15 @@
 module GameRef
     def winner?
-        winner = nil
-        winner = check_rows 
-        winner = check_columns if winner == false
-        winner = check_diagonally_ascending if winner == false
-        winner = check_diagonally_descending if winner == false
-        winner
+        @winner = nil
+        @winner = check_rows 
+        @winner = check_columns if winner == false
+        @winner = check_diagonally_ascending if winner == false
+        @winner = check_diagonally_descending if winner == false
+        @winner
     end
 
     def winners_name
-
+        @players[0].character == @winner ? @players[0].name : @players[1].name
     end
 
     private 
@@ -38,10 +38,30 @@ module GameRef
     end
 
     def check_diagonally_ascending
+        for row in 0..2 do
+            for column in 0..3 do 
+                row_data = get_ascending_diagonal_row([row,column]).map(&:character).join
+                player1_character = @players[0].character
+                player2_character = @players[1].character
+
+                return player1_character if row_data.include?(player1_character * 4)
+                return player2_character if row_data.include?(player2_character * 4)
+            end
+        end 
         false
     end
 
     def check_diagonally_descending
+        for row in 3..5 do
+            for column in 0..3 do 
+                row_data = get_descending_diagonal_row([row,column]).map(&:character).join
+                player1_character = @players[0].character
+                player2_character = @players[1].character
+
+                return player1_character if row_data.include?(player1_character * 4)
+                return player2_character if row_data.include?(player2_character * 4)
+            end
+        end 
         false
     end
 end
