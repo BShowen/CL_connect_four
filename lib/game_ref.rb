5d686a5d9a18e4c6014be90dd6e57ffa335fa24e
Bradley Showen <BShowen@me.com>
2019-07-17 19:56:23 -0400

    refactor row methods to return true instead of player characters

--- lib/game_ref.rb
+++ lib/game_ref.rb
@@ -1,60 +1,73 @@
 module GameRef
     def winner?
-        @winner = check_rows 
-        @winner = check_columns if winner == false
-        @winner = check_diagonally_ascending if winner == false
-        @winner = check_diagonally_descending if winner == false
-        @winner
-    end
-
-    def winners_name
-        @player1.character == @winner ? @player1.name : @player2.name
+        [check_rows,check_columns, check_ascending_row, check_descending_row].find do |method| 
+            method == true
+        end
     end
 
     private 
     def check_rows
         for row in 0..5
-            row = get_row(row).map(&:character).join
-            result = check_row_or_column(row)
-            return result if result != nil
+            row_data = get_row(row).map(&:character).join
+            if check_row_or_column(row_data) 
+                return true 
+            else
+                next 
+            end
         end
         false
     end
 
     def check_columns
         for column in 0..6
-            column = get_column(column).map(&:character).join
-            result = check_row_or_column(column)
-            return result if result != nil
+            column_data = get_column(column).map(&:character).join
+            if check_row_or_column(column_data) 
+                return true 
+            else
+                next 
+            end
         end
         false
     end
 
-    def check_diagonally_ascending
+    def check_ascending_row
         for row in 0..2 do
             for column in 0..3 do 
-                row_data = get_ascending_diagonal_row([row,column]).map(&:character).join
-                result = check_row_or_column(row_data)
-                return result if result != nil
+                row_data = get_ascending_row([row,column]).map(&:character).join
+                if check_row_or_column(row_data) 
+                    return true 
+                else
+                    next 
+                end
             end
         end 
         false
     end
 
-    def check_diagonally_descending
+    def check_descending_row
         for row in 3..5 do
             for column in 0..3 do 
-                row_data = get_descending_diagonal_row([row,column]).map(&:character).join
-                result = check_row_or_column(row_data)
-                return result if result != nil
+                row_data = get_descending_row([row,column]).map(&:character).join
+                if check_row_or_column(row_data) 
+                    return true 
+                else
+                    next 
+                end
             end
         end 
         false
     end
 
     def check_row_or_column(row_or_col)
-        return @player1.character if row_or_col.include?(@player1.character * 4)
-        return @player2.character if row_or_col.include?(@player2.character * 4)
+        if row_or_col.include?(@player1.character * 4)
+            @winners_name = @player1.name
+            return true
+        elsif row_or_col.include?(@player2.character * 4)
+            @winners_name = @player2.name
+            return true
+        end
+        false
     end
 end
 
+#you are here. Notes for when you get back. I want to change #winner? to a function that sets the @winner variable to the name of the winner if there is one and the returns true. If there is no winner then dont touch @winner, just leave it nil (its default value) and return false. This way I can simply call #winner? and if it returns true I can simply display @winner. Also I may want to change @winner to @winners_name 
