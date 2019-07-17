6a8d9dceb118fa264da00382f5c29afb18899591
Bradley Showen <BShowen@me.com>
2019-07-16 20:04:21 -0400

    refactor GameRef to work with GameBoard @player1 @player2 instance var's

--- lib/game_ref.rb
+++ lib/game_ref.rb
@@ -8,18 +8,17 @@ module GameRef
     end
 
     def winners_name
-        @players[0].character == @winner ? @players[0].name : @players[1].name
+        # @players[0].character == @winner ? @players[0].name : @players[1].name
+        @player1.character == @winner ? @player1.name : @player2.name
     end
 
     private 
     def check_rows
         for row in 0..5
             row = get_row(row).map(&:character).join
-            player1_character = @players[0].character
-            player2_character = @players[1].character
     
-            return player1_character if row.include?(player1_character * 4)
-            return player2_character if row.include?(player2_character * 4)
+            return @player1.character if row.include?(@player1.character * 4)
+            return @player2.character if row.include?(@player2.character * 4)
         end
         false
     end
@@ -27,11 +26,9 @@ module GameRef
     def check_columns
         for column in 0..6
             column = get_column(column).map(&:character).join
-            player1_character = @players[0].character
-            player2_character = @players[1].character
     
-            return player1_character if column.include?(player1_character * 4)
-            return player2_character if column.include?(player2_character * 4)
+            return @player1.character if column.include?(@player1.character * 4)
+            return @player2.character if column.include?(@player2.character * 4)
         end
         false
     end
@@ -40,11 +37,9 @@ module GameRef
         for row in 0..2 do
             for column in 0..3 do 
                 row_data = get_ascending_diagonal_row([row,column]).map(&:character).join
-                player1_character = @players[0].character
-                player2_character = @players[1].character
 
-                return player1_character if row_data.include?(player1_character * 4)
-                return player2_character if row_data.include?(player2_character * 4)
+                return @player1.character if row_data.include?(@player1.character * 4)
+                return @player2.character if row_data.include?(@player2.character * 4)
             end
         end 
         false
@@ -54,11 +49,9 @@ module GameRef
         for row in 3..5 do
             for column in 0..3 do 
                 row_data = get_descending_diagonal_row([row,column]).map(&:character).join
-                player1_character = @players[0].character
-                player2_character = @players[1].character
 
-                return player1_character if row_data.include?(player1_character * 4)
-                return player2_character if row_data.include?(player2_character * 4)
+                return @player1.character if row_data.include?(@player1.character * 4)
+                return @player2.character if row_data.include?(@player2.character * 4)
             end
         end 
         false
