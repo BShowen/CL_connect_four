e5d6c86d310b147ea5889de5e71359e90d3f3240
Bradley Showen <BShowen@me.com>
2019-07-18 21:07:52 -0400

    game is functioning. main.rb needs refactoring

--- lib/main.rb
+++ lib/main.rb
@@ -16,15 +16,20 @@ game_board = GameBoard.new(player1, player2)
 loop do
     clear_screen
     puts game_board.display.lines.map!{|line| line.strip.center(120)}.join("\n")
-    break if game_board.winner?
+    break if game_board.winner? || game_board.tie?
     puts "#{player1.name}, whats your move?"
     game_board.drop_piece(Player.move, player1.character)
     clear_screen
     puts game_board.display.lines.map!{|line| line.strip.center(120)}.join("\n")
-    break if game_board.winner?
+    break if game_board.winner? || game_board.tie?
     puts "#{player2.name}, whats your move?"
     game_board.drop_piece(Player.move, player2.character)
 end
 
 puts "GAME OVER".center(120)
-puts "#{game_board.winners_name} won!".center(120)
+
+if game_board.winners_name
+    puts "#{game_board.winners_name} won!".center(120)
+elsif game_board.tie?
+    puts "It was a tie".center(120)
+end
