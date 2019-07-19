0052b1c77b6dc187b85ecf7068978482981bc9eb
Bradley Showen <BShowen@me.com>
2019-07-18 21:05:44 -0400

    green light on tests

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -57,7 +57,6 @@ RSpec.describe GameBoard do
     end
 
     context "#get_ascending_row" do 
-
         it "returns the row, starting from the coordinates of any square" do
             create_ascending_diagonal_row
             expect(@board.get_ascending_row([0,0]).map(&:character)).to eql(["X","X","O","X","X","X"])
@@ -98,46 +97,46 @@ RSpec.describe GameBoard do
     end
 
     context "#get_descending_row" do 
-    it "returns the row, starting from the coordinates of any square" do
-        create_descending_diagonal_row
-        
-        expect(@board.get_descending_row([5,1]).map(&:character)).to eql(["X","X","X","O","X","X"])
-    end
+        it "returns the row, starting from the coordinates of any square" do
+            create_descending_diagonal_row
+            
+            expect(@board.get_descending_row([5,1]).map(&:character)).to eql(["X","X","X","O","X","X"])
+        end
 
-    it "same test as above with different starting position" do
-         
-        expect(@board.get_descending_row([5,0]).map(&:character)).to eql([" "," "," "," "," "," ",])
-    end 
+        it "same test as above with different starting position" do
+            
+            expect(@board.get_descending_row([5,0]).map(&:character)).to eql([" "," "," "," "," "," ",])
+        end 
 
-    it "returns a partially populated row" do 
-        @board.get_row(1)[5].character = "O"
-        @board.get_row(3)[3].character = "X"
-        @board.get_row(5)[1].character = "O"
-        
-        expect(@board.get_descending_row([5,1]).map(&:character)).to eql(["O"," ","X"," ","O"," ",])
-    end
+        it "returns a partially populated row" do 
+            @board.get_row(1)[5].character = "O"
+            @board.get_row(3)[3].character = "X"
+            @board.get_row(5)[1].character = "O"
+            
+            expect(@board.get_descending_row([5,1]).map(&:character)).to eql(["O"," ","X"," ","O"," ",])
+        end
 
-    it "handles an edge case where the initial square is near a corner" do 
-        @board.get_row(4)[6].character = "X"
-        @board.get_row(5)[5].character = "O"
-        
-        expect(@board.get_descending_row([5,5]).map(&:character)).to eql(["O","X"])
-    end
+        it "handles an edge case where the initial square is near a corner" do 
+            @board.get_row(4)[6].character = "X"
+            @board.get_row(5)[5].character = "O"
+            
+            expect(@board.get_descending_row([5,5]).map(&:character)).to eql(["O","X"])
+        end
 
-    it "handles an edge case where the initial square is near another corner" do 
-        @board.get_row(0)[1].character = "X"
-        @board.get_row(1)[0].character = "O"
-        
-        expect(@board.get_descending_row([1,0]).map(&:character)).to eql(["O","X"])
-    end
+        it "handles an edge case where the initial square is near another corner" do 
+            @board.get_row(0)[1].character = "X"
+            @board.get_row(1)[0].character = "O"
+            
+            expect(@board.get_descending_row([1,0]).map(&:character)).to eql(["O","X"])
+        end
 
-    it "handles an edge case where the starting square is in the middle of the board" do 
-        @board.get_row(3)[3].character = "X"
-        @board.get_row(2)[4].character = "O"
-        @board.get_row(1)[5].character = "X"
-        
-        expect(@board.get_descending_row([3,3]).map(&:character)).to eql(["X","O","X"," "])
-    end
+        it "handles an edge case where the starting square is in the middle of the board" do 
+            @board.get_row(3)[3].character = "X"
+            @board.get_row(2)[4].character = "O"
+            @board.get_row(1)[5].character = "X"
+            
+            expect(@board.get_descending_row([3,3]).map(&:character)).to eql(["X","O","X"," "])
+        end
     end
  
     context "#drop_piece" do 
